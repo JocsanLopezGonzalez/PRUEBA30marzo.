1920d1d641618088783a92a15a1c7093816c5369
Andre <Andre@192.168.56.1>
2020-04-02 11:24:34 -0600

    Prueba Repositorio 30 marzo

---
+++ Prueba30marzo/src/Prueba30marzo.java
@@ -0,0 +1,20 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Andre
+ */
+public class Prueba30marzo {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Andreina Alva");
+        System.out.println("Zona 3, Huehuetenango");
+        System.out.println("41807261");
+    }
+    
+}
