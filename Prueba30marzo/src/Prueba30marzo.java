0d8cedcc0ebea6717806c1c9f1c4e237b9a77f93
Carlos Andrés Miranda Méndez <62572969+CarlosMM28@users.noreply.github.com>
2020-04-02 12:11:51 -0600

    Update Prueba30marzo.java

--- Prueba30marzo/src/Prueba30marzo.java
+++ Prueba30marzo/src/Prueba30marzo.java
@@ -19,6 +19,10 @@ public class Prueba30marzo {
         System.out.println("Daniel Alonzo Herrera ");
         System.out.println("7a. AVE 9-81 Zona 5, Huehuetenango, Huehuetenango ");
         System.out.println("3059-6671 ");
+        
+        System.out.println("Carlos Andrés Miranda Méndez");
+        System.out.println("1a Calle A, Colonia Villas del Magisterio Zona 9");
+        System.out.println("58385781");
     }
     
 }
